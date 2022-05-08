38962d40e7f675a850fa95ececa890f3d3075337
AlejandroClift <71460687+AlejandroClift@users.noreply.github.com>
2022-05-08 18:53:53 -0500

    jest added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Button.scss"
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="myButton"><b>{props.label}</b></button>;
 };
 
 export default Button;
